35be1af7e761cce26c28318903e7cc10325743d1
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-08-29 16:49:05 +0200

    turn

--- client/src/client.js
+++ client/src/client.js
@@ -18,12 +18,47 @@ const onChatSubmitted = (e) => {
   sock.emit('message', text);
 }
 
+const getBoard = (canvas) => {
+  const contexte = canvas.getContext('2d');
+
+  const fillRect = (x, y, color) => {
+    contexte.fillStyle = color;
+    contexte.fillRect(x -10, y - 10, 20, 20)
+  }
+
+  return { fillRect };
+}
+
+// The coordinate of the click within the canvas
+const getClickCoordinates = (element, event) => {
+  //.getBoundingClientRect = Return object about the size and position relarive to the viewport
+  const {top, left} = element.getBoundingClientRect();
+  const {clientX, clientY} = event;
+
+  return {
+    x: clientX - left,
+    y: clientY - top
+  }
+};
+
 (() => {
+  const canvas = document.getElementById('canvas');
+  const {fillRect} = getBoard(canvas);
   const sock = io();
 
+  const onClick = (e) => {
+    const {x, y} = getClickCoordinates(canvas, e);
+    // Render the rectangle
+    sock.emit('turn', {x , y});
+  }
+
   sock.on('message',log);
+  sock.on('turn', ({x, y}) => fillRect(x, y))
+
 
   document
     .querySelector('#chat-form')
     .addEventListener('submit', onChatSubmitted);
+
+  canvas.addEventListener('click', onClick)
 })();
